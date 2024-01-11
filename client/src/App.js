86e3c709de3f9e3e76453ce2d7ec1e0b335a6a88
reservetempo <dominic.riffou.loomes@hotmail.com>
2024-01-11 15:57:23 -0500

    create account, make username/ root directory

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,24 @@ import Homepage from "./components/Homepage";
 import Signin from "./components/Signin";
 import Directory from "./components/Directory"
 import NotFound from "./components/NotFound";
-import { useEffect, useState } from "react";
-import makeFetchRequest from "./utils/make-fetch-request";
-import { getDirectory } from "./service/handleGet";
-import { useAuth0 } from "@auth0/auth0-react";
+import { UserProvider } from "./components/UserContext";
 
 const App = () => {
-  const { user, isAuthenticated } = useAuth0();
 
   return (
     <div className="App">
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/:dir0?/:dir1?/:dir2?/:dir3?/:dir4?/:dir5?/:dir6?/:dir7?/:dir8?/:dir9?" element={<Directory />}/>
-            <Route path="/notfound" element={<NotFound />} />
-          </Routes>
-        </Router>
+        <UserProvider>
+          <Router>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/:dir0?/:dir1?/:dir2?/:dir3?/:dir4?/:dir5?/:dir6?/:dir7?/:dir8?/:dir9?" element={<Directory />}/>
+              <Route path="/notfound" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </UserProvider>
+
     </div>
   );
 }
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,8 +1,59 @@
+import makeFetchRequest from "../utils/make-fetch-request";
+import { getDirectories, getDirectory } from "../service/handleGet";
+import { useEffect, useRef, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { makeDirectory } from "../service/handlePost";
+import CreateDirectory from "./CreateDirectory";
 
 const Homepage = () => {
+    const [users, setUsers] = useState(null);
+    const { user, isAuthenticated } = useAuth0();
+    // only run if context is null
+    const [checkedAccount, setCheckedAccount] = useState(false);
+    const [hasAccount, setHasAccount] = useState(false);
+    const usernameRef = useRef(null);
+
+    const getUsers = async () => {
+        const result = await makeFetchRequest(() => getDirectories());
+        setUsers(result.result)
+    };
+
+    const getUser = async () => {
+        const result = await makeFetchRequest(() => getDirectory(user.email));
+        if (result.result) {
+            setHasAccount(true);
+        }
+        setCheckedAccount(true)
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, [])
+
+    useEffect(() => {
+        if (user) getUser();
+    }, [user])
 
     return (
-        <h1>HOMEPAGE</h1>
+        <main>
+            <h1>homepage</h1>
+            {isAuthenticated && checkedAccount && !hasAccount &&
+            <CreateDirectory 
+                userEmail={user.email} 
+                setHasAccount={setHasAccount} 
+                getUsers={getUsers}
+            />
+
+            }
+            <ul>
+            {users &&
+            users.map((u) => {
+                return (
+                    <li key={u.username}><p>{u.username}</p></li>
+                )
+            })}
+            </ul>
+        </main>
     )
 }
 
